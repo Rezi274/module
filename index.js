fa59816ad2a52c974eb84ab987182db49832e4d1
Rezi274 <97589286+Rezi274@users.noreply.github.com>
2022-02-15 09:52:55 +0700

    mengubah source code

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
-const coba = require('./module');
-console.log(new coba.Table(),coba.judulTabel,coba.badanTabel,coba.proses,coba.table);
+/*const coba = require('./module');
+console.log(new coba.Table(),coba.judulTabel,coba.badanTabel,coba.proses,coba.table);*/
+
+
+import {Table, judulTabel, badanTabel, proses, table, rezi} from "./module";
+
+console.log(new Table, judulTabel(), badanTabel(), proses(), table(),rezi);
